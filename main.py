6641a47cd6a58ba5f35a51893d4c104cef759309
dilyanabodurova <diliana.bodurova@abv.bg>
2025-12-28 18:53:34 +0200

    add save func

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 from tkinter import *
 
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+
+def save_data():
+    website = entry_website.get()
+    email = entry_email.get()
+    password = entry_password.get()
+
+    with open("data.txt", "a") as f:
+        f.write(f'{website} | {email} | {password}\n')
+
+    entry_website.delete(0, END)
+    entry_email.delete(0, END)
+    entry_password.delete(0, END)
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -15,20 +28,22 @@ security_png = PhotoImage(file='logo.png')
 canvas.create_image(100, 100, image=security_png)
 canvas.grid(column=1, row=0)
 
-label_website = Label (text='Website:')
+label_website = Label(text='Website:')
 label_website.grid(column=0, row=1)
 
-label_email = Label (text='Email/Username:')
+label_email = Label(text='Email/Username:')
 label_email.grid(column=0, row=2)
 
-label_password = Label (text='Password:')
+label_password = Label(text='Password:')
 label_password.grid(column=0, row=3)
 
 entry_website = Entry(width=38)
 entry_website.grid(column=1, row=1, columnspan=2)
+entry_website.focus()
 
 entry_email = Entry(width=38)
 entry_email.grid(column=1, row=2, columnspan=2)
+entry_email.insert(0, 'dilyana.m.bodurova@gmail.com')
 
 entry_password = Entry(width=21)
 entry_password.grid(column=1, row=3)
@@ -36,7 +51,7 @@ entry_password.grid(column=1, row=3)
 button_generate_password = Button(text='Generate Password')
 button_generate_password.grid(column=2, row=3)
 
-button_add = Button(text='Add',width=36)
+button_add = Button(text='Add', width=36, command=save_data)
 button_add.grid(column=1, row=4, columnspan=2)
 
 window.mainloop()
